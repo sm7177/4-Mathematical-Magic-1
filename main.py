cf490d3f263da696e86a009e3aec40b33f9b9a04
sm7177 <subrinamaharjan2020@gmail.com>
2024-10-30 17:00:17 +0545

    ..

--- main.py
+++ main.py
@@ -18,10 +18,29 @@
 
 
 
-def pf(n):
-    print("The factors are:")
-    for i in range(1,n+1):
-        if n%i == 0:
-            print(i)
-num=int(input("Enter a number:"))
-pf(num)
+# def pf(n):
+#     print("The factors are:")
+#     for i in range(1,n+1):
+#         if n%i == 0:
+#             print(i)
+# num=int(input("Enter a number:"))
+# pf(num)
+
+
+
+
+
+
+
+def romantoint(n):
+    roman={"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+    int_form=0
+    for i in range(len(n)):
+        if i+1<len(n) and roman[n[i]]<roman[n[i+1]]:
+            int_form-=roman[n[i]]
+        else:
+            int_form+=roman[n[i]]
+    return int_form
+n=input("Enter a roman number:")
+print("Integer form of",n,"is",romantoint(n))
+    
